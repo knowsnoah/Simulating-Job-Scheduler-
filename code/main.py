f6c6d7bdc929818f0ba99f49dc499d0f27c5b4e0
noah <ojedanoah24@gmail.com>
2026-03-17 16:53:39 -0700

    update main.py

--- code/main.py
+++ code/main.py
@@ -3,6 +3,7 @@
 
 #imports to get algorithm from their respective files
 import random
+import copy
 from fcfs import fcfs
 from sjf import sjf
 from rr2 import rr2
@@ -40,4 +41,55 @@ def write_jobs_to_file(jobs, filename="jobs.txt"):
             f.write(f"Job{job['id']}\n")
             f.write(f"{job['burstTime']}\n")
 
-#generate jobs for testing
+
+
+#GENERATING JOBS FOR TESTING - PART 3
+def run_experiment():
+    #number of trials
+    trial_size = 20
+    job_sizes = [5, 10, 15]
+
+    for size in job_sizes:
+        #variable to hold the results
+        fcfs_total = 0
+        sjf_total = 0
+        rr2_total = 0 
+        rr5_total = 0
+
+        #20 trails for each job size and schedule algorithm 
+        for trial in range(trial_size):
+            jobs = generate_jobs(size) #generating 5, 10, and 15 jobs 
+
+            #using deepcopy to make sure each one gets the same input of jobs
+            fcfs_results = fcfs(copy.deepcopy(jobs))
+            sjf_results = sjf(copy.deepcopy(jobs))
+            rr2_results = rr2(copy.deepcopy(jobs))
+            rr5_results = rr5(copy.deepcopy(jobs))
+
+            fcfs_total = average_turnaround(fcfs_results)
+            sjf_total = average_turnaround(sjf_results)
+            rr2_total = average_turnaround(rr2_results)
+            rr5_total = average_turnaround(rr5_results)
+
+        #computing the average of the averages after 20 trials
+        fcfs_avg = fcfs_total / trial
+        sjf_avg = sjf_total / trial
+        rr2_avg = rr2_total / trial
+        rr5_avg = rr5_total / trial
+
+        #returning the averages of each job size[5, 10, 15] for each schedule algorithm 
+        print("Input Size | FCFS Avg TAT | SJF Avg TAT | RR-2 Avg TAT | RR-5 Avg TAT")
+        print(f"{size:^10} | {fcfs_avg:^12.2f} | {sjf_avg:^11.2f} | {rr2_avg:^12.2f} | {rr5_avg:^12.2f}")
+
+
+#RUNNING run_experiment
+if __name__ == "__main__":
+    run_experiment()
+
+
+
+            
+
+
+
+
