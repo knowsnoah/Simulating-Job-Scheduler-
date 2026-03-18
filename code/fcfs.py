f7313c4ee57a27a9b7191930f9a727a06078db1f
noah <ojedanoah24@gmail.com>
2026-03-17 17:45:21 -0700

    updated fcfs.py

--- code/fcfs.py
+++ code/fcfs.py
@@ -3,7 +3,7 @@
 
 from collections import deque
 
-#Function
+#Function for the first come first serve algorithm 
 def fcfs(jobs):
     #sorting the jobs by arrival time
     jobs = sorted(jobs, key=lambda x: x['arrivalTime'])
@@ -16,15 +16,19 @@ def fcfs(jobs):
 
     results = []
 
+    #loop until all of the jobs are completed
     while completed < n:
+        #adding all of the jobs that have the arrival time by the current time into the queue
         while i < n and jobs[i]['arrivalTime'] <= currentTime:
             readyQueue.append(jobs[i])
             i = i + 1
+        #whille there are jobs in the queue
         if readyQueue:
             job = readyQueue.popleft()
             start = max(currentTime, job['arrivalTime']) #picks the max out of the current time and the arrival time of the job
             finish = start + job['burstTime']
 
+            #appending the result along with its updated waitingTime and turnaroundTime 
             results.append({
                 'id': job.get('id'),
                 'arrivalTime' : job['arrivalTime'],
@@ -33,7 +37,9 @@ def fcfs(jobs):
                 'waitingTime' : start - job['arrivalTime'],
                 'turnaroundTime' : finish - job['arrivalTime']
             })
+            #updating the current time
             currentTime = finish
+            #adding to number of jobs completed 
             completed +=1
 
         else:
